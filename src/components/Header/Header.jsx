4659886716afa8199ae712d4cbca84c3631f55c0
Arnaud <arnaud.obri@proton.me>
2024-09-27 11:53:59 +0200

    update github link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ const Header = ({ className }) => {
   return (
     <header className={className}>
       <img src="/logo.png" alt="MoviePicker logo" />
-      <a href="https://github.com/arobri67" rel="noopener" target="_blank">
+      <a
+        href="https://github.com/arobri67/movie-picker"
+        rel="noopener"
+        target="_blank"
+      >
         View on Github
       </a>
     </header>
